d3a1e958dd185c040fb681ab15e4cf983eed4a9f
Jahangir-Alam-Noman <jahangir2km@gmail>
2021-11-10 08:19:23 +0600

    project test

--- src/App.js
+++ src/App.js
@@ -6,22 +6,9 @@ function App() {
   return (
     <div className="App">
       <h2>Hello components</h2>
-      <button type="button" class="btn btn-primary">Primary</button>
-      <button type="button" class="btn btn-secondary">Secondary</button>
-      <button type="button" class="btn btn-success">Success</button>
-      <button type="button" class="btn btn-danger">Danger</button>
-      <button type="button" class="btn btn-warning">Warning</button>
-      <button type="button" class="btn btn-info">Info</button>
-      <button type="button" class="btn btn-light">Light</button>
-      <button type="button" class="btn btn-dark">Dark</button>
-
-      <button type="button" class="btn btn-link">Link</button>
-      <i class="fa fa-facebook" aria-hidden="true"></i>
 
       <Row className="mx-0">
         <Button as={Col} variant="primary">Button #1</Button>
-        <Button as={Col} variant="secondary" className="mx-2">Button #2</Button>
-        <Button as={Col} variant="success">Button #3</Button>
       </Row>
 
 
